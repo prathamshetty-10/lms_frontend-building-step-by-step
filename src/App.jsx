9c2b331be6c51e82f2501e51fdfb9c871984dd3c
Pratham shetty <prathamshetty484@gmail.com>
2024-01-12 21:12:58 +0530

    slice store n all basics template added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+
+
+import { Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    hi
+    <Routes>
+    </Routes>
       
     </>
   )
