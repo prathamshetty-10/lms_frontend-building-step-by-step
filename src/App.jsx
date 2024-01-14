863d4b8f36d2e7a93c1326753b954522f9096001
Pratham shetty <prathamshetty484@gmail.com>
2024-01-14 19:05:11 +0530

    connected front to back and created signup page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/footer'
 import HomePage from "./Pages/HomePage"
 import AboutUs from "./Pages/AboutUs"
 import NotFound from './Pages/NotFound'
+import Signup from './Pages/SignupPage'
 
 function App() {
   
@@ -16,6 +17,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
       <Route path="/about" element={<AboutUs/>}></Route>
+      <Route path="/signup" element ={<Signup/>}></Route>
       <Route path="*" element ={<NotFound/>}></Route>
     </Routes>
     </>
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -1,10 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import axiosInstance from "../../helpers/axiosInstance";
+
 const initialState={
     isLoggedIn:localStorage.getItem('isLoggedIn')|| false,
     role:localStorage.getItem("role")|| "",
     data:localStorage.getItem("data")||{}
 };//this is the initial state of the Auth slice
+export const createAccount=createAsyncThunk("/auth/signup",async(data)=>{
+    try{
+        const res=axiosInstance.post("user/register",data);//this is the route given in backend
+        toast.promise(res,{
+            loading:"wait! creating your account",
+            success:(data)=>{
+                return data?.data.message;
+            },
+            error:"Failed to create account"
+        });
+        return (await res).data;
 
+
+    }catch(error){
+        toast.error(error?.response?.data?.message);//to send the error as alert message
+
+    }
+})
 //creating the Auth slice
 const authSlice=createSlice({
     name:"auth",
